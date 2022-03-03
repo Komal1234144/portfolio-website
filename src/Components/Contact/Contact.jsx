e9f584fb95cf41661922edf52dc75a9f0615ff7d
Komal1234144 <tolambia.komal5@gmail.com>
2022-03-03 13:40:35 +0530

    commit

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -49,7 +49,7 @@ function handleSubmit(e) {
   return (
     <div className='contact' id="contact">
        <div className='contact__left'>
-         <img src="./assets/shake.svg" alt="handshake-picture"/>
+         <img src="./assets/shake.svg" alt="handshake"/>
          <div>
             <span><a className='contactIcons' href='https://github.com/Komal1234144'><GitHub className='icon'/></a></span>
             <span><a className='contactIcons' href='mailto:tolambia.komal5@gmail.com'><MailOutline className='icon'/></a></span>
